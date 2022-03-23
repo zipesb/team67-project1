e4db7cd314d5d3a2dd7a0a257df9585d58d4170c
Acornidehuber <alex13huber@gmail.com>
2022-03-22 20:36:40 -0400

    Add Axios, React Router deps & made CreatePage, ClassPage components w/ routes

--- classhub/src/App.js
+++ classhub/src/App.js
@@ -1,24 +1,35 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
+import CreatePage from "./components/createpage.component";
+import ClassPage from "./components/classpage.component";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+       </header>*/}
+       <Routes>
+         <Route path="/create" exact element={<CreatePage/>} />
+         <Route path="/view/:id" element={<ClassPage/>} />
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ classhub/src/components/createpage.component.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'; // Hooks for functional components
+import axios from 'axios'; // Cleaner way of sending http requests
+import { useNavigate } from 'react-router-dom';
+
+
+
+const CreatePage = () =>
+{
+    const [ pagename, setPageName ] = useState('');
+    const [ pagedescription, setPageDescription ] = useState('');
+    // TODO: What else do we want..? Some content to add in advance? Tags? Description?
+
+    const navigate = useNavigate();
+
+    const onChangePageName = (e) => setPageName(e.target.value);
+
+    const onChangePageDescription = (e) => setPageDescription(e.target.value);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // Store new paga data in JSON
+        const newpage = {
+            name: pagename,
+            description: pagedescription,
+            content: ''
+        }
+
+        // Send HTTP request using axios
+        axios.post('http://localhost:5000/create', newpage)
+            .then(res => {
+                console.log(res.data);
+                navigate('/view/'+res.data._id);
+            });
+
+        // Reset values
+        //setPageName('');
+        //setPageDescription('');
+    }
+
+    return (
+        <>
+            <h1>Create a Class Page </h1>
+            <form action="">
+                <input  type="text" 
+                        placeholder="Name" 
+                        value={pagename} 
+                        onChange={onChangePageName} 
+                        />
+                <br/>
+                <input  type="text" 
+                        placeholder="Description" 
+                        value={pagedescription} 
+                        onChange={onChangePageDescription} />
+                <br/>
+                <button type="submit" 
+                        onClick={onSubmit}>Create</button>
+            </form>
+        </>
+    )
+
+}
+
+export default CreatePage;
