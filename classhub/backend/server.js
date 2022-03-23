ecf656004a8f836845745dbac357def6d558a40a
Acornidehuber <alex13huber@gmail.com>
2022-03-22 20:34:15 -0400

    Add CreatePage and ClassPage components w/ routes

--- classhub/backend/server.js
+++ classhub/backend/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+let Class = require('./class.model');
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true}
 );
@@ -20,4 +22,31 @@ connection.once('open', () => {
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
+})
+
+
+// Endpoint to add a new class
+app.post("/create", async (req,resp) => {
+    // Create a new Class document using the JSON of request's body
+    let newclass = new Class(req.body);
+    // Save the document to database
+    newclass.save()
+      // Callback functions
+      .then(newclass => {
+        resp.status(200).send('Class added succesfully');
+      })
+      .catch(err => {
+        resp.status(400).send('Failed to add class');
+      });
+});
+
+// Endpoint to get an existing class' info
+app.get("/:id", async (req,resp) => {
+    let id = req.params.id;
+    Class.findById(id, function(err, foundclass) {
+      if(!foundclass)
+        resp.status(404).send('Class data not found');
+      else
+        resp.json(foundclass);
+    });
 })
