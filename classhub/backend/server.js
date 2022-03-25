7521bc1346765e229adba15dcbffc82d4714b75e
ngiridharan <73603346+ngiridharan@users.noreply.github.com>
2022-03-25 15:31:03 -0400

    added simple post and get for users and classes

--- classhub/backend/server.js
+++ classhub/backend/server.js
@@ -13,6 +13,45 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true}
 );
+
+app.get("/getUsers", (req, res)=>{
+  UserModel.find({}, (err, result)=>{
+      if(err){
+          res.json(err);
+      } else {
+          res.json(result);
+      }
+  } );
+});
+
+app.post("/createUser", async(req,res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+})
+app.get("/getClasses", (req, res)=>{
+  ClassModel.find({}, (err, result)=>{
+      if(err){
+          res.json(err);
+      } else {
+          res.json(result);
+      }
+  } );
+});
+
+app.post("/createClass", async(req,res) => {
+  const class1 = req.body;
+  const newClass = new ClassModel(user);
+  await newClass.save();
+
+  res.json(class1);
+})
+
+
+
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
