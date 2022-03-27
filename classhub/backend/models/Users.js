bb50e7efb7598952936453f7d1e7e55de85952d7
Benjamin Zipes <zipesb@gmail.com>
2022-03-26 22:12:26 -0400

    fixed to accomodate existing Users schema

--- classhub/backend/models/Users.js
+++ classhub/backend/models/Users.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     name: {
         type: String,
         required: true,
@@ -8,11 +13,15 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
-    },    
+    },
+    classes: {
+        type: Array,
+    },
 }) 
 
 const UserModel = mongoose.model("users", UserSchema);
--- classhub/backend/routes/api/users.js
+++ classhub/backend/routes/api/users.js
@@ -26,6 +26,7 @@ router.post("/register", (req, res) => {
         return res.status(400).json({ email: "Email already exists" });
       } else {
         const newUser = new User({
+          username: req.body.username,
           name: req.body.name,
           email: req.body.email,
           password: req.body.password
