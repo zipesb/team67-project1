0ab82352d7ec4c20aee111ba28233dae98e457c2
ngiridharan <73603346+ngiridharan@users.noreply.github.com>
2022-03-27 17:10:47 -0400

    Search bar functionality added

---
+++ classhub/src/components/SearchBar.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react'
+import "./SearchBar.css"
+import SearchIcon from "@material-ui/icons/Search"
+import CloseIcon from "@material-ui/icons/Close"
+
+
+
+
+function SearchBar({placeholder, data}) {
+    
+
+    const [filteredData, setFilteredData] = useState([])
+    const [wordEntered, setWordEntered] = useState("")
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord)
+        const newFilter = data.filter((value)=>{
+            return value.name.toLowerCase().includes(searchWord.toLowerCase());
+        })
+        if(searchWord === ""){
+            setFilteredData([])
+        }
+        else{
+        setFilteredData(newFilter)
+        }
+    }
+
+    const clearInput = () =>{
+        setFilteredData([]);
+        setWordEntered("")
+    } 
+    return (
+        <div className = "search">
+            <div className="searchInputs">
+                <input type = "text" placeholder = {placeholder} value ={wordEntered} onChange = {handleFilter}/>
+                <div className = "searchIcon">
+                    {filteredData.length == 0 ? <SearchIcon/> : <CloseIcon id = "clearBtn" onClick = {clearInput}
+                    /> }
+                </div>
+            </div>
+            {filteredData.length!= 0 &&(
+            <div className="dataResult">
+                {filteredData.slice(0,15).map((value, key)=>{
+                    return( <a className='dataItem' href={value.htmlContent} target = "_blank"> 
+                        <p>{value.name}</p>
+                    </a>
+                    );
+                })} 
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default SearchBar
