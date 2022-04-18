d733507f5d6f7bb37481b2f89e274eb8c013a9a7
Acornidehuber <alex13huber@gmail.com>
2022-04-18 05:44:13 -0400

    Store username

--- classhub/src/components/ClassPage.js
+++ classhub/src/components/ClassPage.js
@@ -9,17 +9,21 @@ import PageViewer from "./PageViewer"
 const ClassPage = () =>
 {
     const [ pageclass, setClass ] = useState({});
+    const [ isEditor, setIsEditor ] = useState(false);
     const [ isEditing, setEditing ] = useState(false);
+
+    const loggedInUser = localStorage.getItem("username");
     const { id } = useParams();
 
     const loadClass = () => {
         axios.get('http://localhost:5000/getClass/'+id)
             .then(resp => {
                 setClass(resp.data)
+                setIsEditor(resp.data.editors.includes(loggedInUser));
             })
             .catch(function(err) {
                 console.log(err);
-            })
+            });
     }
 
     const toggleEdit = () => {
@@ -36,7 +40,7 @@ const ClassPage = () =>
         <div>
             {isEditing  ? <PageEditor class={pageclass} onclick={toggleEdit}/> 
                         : <PageViewer class={pageclass} onclick={toggleEdit}/>}
-
+                        
 <ResourceView class={pageclass}/>
         </div>
     )
